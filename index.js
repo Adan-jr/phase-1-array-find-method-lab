2f9a98b9d6dad3ad66b833edb5db8073b6749989
adan <adanabdullahi343@gmail.com>
2024-10-02 22:58:26 +0300

    array

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+// Define the superbowlWin function
+function superbowlWin(record) {
+    // Use the find method to locate the first record where the result is "W"
+    const win = record.find(game => game.result === "W");
+  
+    // If a win is found, return the year, otherwise return undefined
+    return win ? win.year : undefined;
+  }
+  
